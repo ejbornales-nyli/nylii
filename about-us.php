6e291c57f2dafd171809f48af7a962bd0a0c0a23
ejcb1004 <126511954+ejbornales-nyli@users.noreply.github.com>
2023-03-16 10:44:05 +0800

    General updates

--- about-us.php
+++ about-us.php
@@ -40,33 +40,79 @@
 			<div class="about-cover row">
 				<div class="sidebar col-3">
 					<ul>
-						<li><a href="#section1" class="section-link active"><button class="">Humble Beginnings</button></a></li>
-						<li><a href="#section2" class="section-link"><button class="">Mission, Vision, and Advocacy</button></a></li>
+						<li><a href="#section1" class="section-link active"><button class="">Humble
+									Beginnings</button></a>
+						</li>
+						<li><a href="#section2" class="section-link"><button class="">Mission, Vision, and
+									Advocacy</button></a></li>
 						<li><a href="#section3" class="section-link"><button class="">Alumni</button></a></li>
 					</ul>
 				</div>
 
 				<div class="about-content col-8">
-					
+
 					<div class="section active" id="section1">
 						<div class="video-container">
-							<iframe src="https://www.youtube.com/embed/vwT0n5VSOn0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-						  </div>
-						  <br><br>
-						<p>Born from the unwavering history of our forefathers of heroes, sugar barons, migrants, sugar peasants, and unheard minorities, we take pride in the unique Negrosanon blood that beats within our hearts and the sweetness of our race that filled the glory of our lands. NYLI continues to live on, carry on, and pass on to the youth the greatness of the people and the responsibility to co-create a future for everyone.<br><br>
-
-						Back in 2016, Negrosanon Young Leaders Institute was born from a big, ambitious dream to provide an avenue for young leaders to carry out their advocacies outside their schools and to bring about change in their own communities through Negrosanon Young Leaders Summit. NYLS is a project crafted by Teodoro Angelo Villanueva, Junjun Mojica; Youth Empowering Youth Initiative Inc, Sowing Legacy Movement Inc; and other individuals who gathered together for the cause. </p>
-							
-						<img src="/assets/images/placeholder.png" class="img-fluid w-25" alt=""><br><br>
-						<p>On January 2017, the first Negrosanon Young Leaders Summit (NYLS) was staged and participated by the finest young leaders of Negros Occidental, where a wider network has been established and launched the Negrosanon Young Leaders Alumni Association. <br><br>
-
-						On January 2018, the second wave of the summit was implemented. Its outcomes became the kick-off point for the implementers to establish an organization that will cater to the immediate needs of Negrosanon youths, hence Negrosanon Young Leaders Institute. <br><br>
-
-						On December 2018, the Institute was registered under the National Youth Commission’s Youth Organization Registration Program (YORP) as a community-based youth organization. <br><br>
-						
-						On February 2019, Negrosanon Young Leaders Institute Incorporated was formally registered with the Securities and Exchange Commission as a non-stock, non-profit organization. <br><br>
-
-						On 2020, NYLI Inc has been registered with the Department of Social Welfare and Development as a community-based social welfare agency. Today, NYLI has opened more programs for the welfare of the youth of the province.</p>
+							<iframe src="https://www.youtube.com/embed/vwT0n5VSOn0" frameborder="0"
+								allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+								allowfullscreen></iframe>
+						</div>
+						<br>
+						<p>
+							We started with a dream. A dream for our sugar barons, migrants, sugar peasants, and unheard
+							minorities.
+						</p>
+						<p>
+							On December 2018, the Institute was registered under the National Youth Commission’s
+							Youth
+							Organization Registration Program (YORP) as a community-based youth organization., following
+							the
+							support and vision of young leaders from the Negrosanon Young Leaders Summit that started in
+							2017.
+						</p>
+						<p>
+							On February 2019, Negrosanon Young Leaders Institute Incorporated was formally registered
+							with the Securities and Exchange Commission as a non-stock, non-profit organization.
+						</p>
+						<p>
+							On 2020, NYLI Inc. has been registered and licensed with the Department of Social
+							Welfare and
+							Development as a community-based social welfare agency, and in the same year, NYLI Inc
+							received
+							support from the <a href="https://www.facebook.com/consuelozobelalgerfoundation/">Consuelo
+								Zobel Alger Foundation</a> that became instrumental to its growth and
+							development as an organization.
+						</p>
+						<p>
+							On November 2021, the <a href="https://www.facebook.com/NegOcc.Gov">Provincial Government of Negros Occidental</a> through the Local
+							Youth
+							Development Council and the Provincial SK Federation, the Institute has been granted with
+							the
+							deed of
+							usufructuary rights for the Hulot sang Pamatanon and Safe Space for our young people to
+							collaborate
+							and for our humble dreams to expand.
+						</p>
+						<p>
+							Today, NYLI has opened more programs for the welfare of the youth of the province, the
+							region,
+							and the nation with support from Consuelo Zobel Alger Foundation, and other funding partners
+							who
+							believes in the power of young people to make a difference. We have hosted 6 batches of
+							young
+							leaders
+							fellowship all across the province.
+						</p>
+						<p>
+							We are here because we first believed. We are here because we never stopped trying, and you
+							never
+							stopped believing.
+						</p>
+						<p>
+							NYLI Inc. continues to live on, carry on, and pass on to the youth the greatness of the
+							Negrosanon people and responsibility to co-create a future for a sustainable Negros and the
+							Philippines.
+						</p>
 					</div>
 
 					<div class="section" id="section2">
@@ -74,19 +120,26 @@
 							<div class="col-4 sec-col">
 								<img src="/assets/images/placeholder.png" class="img-fluid" alt=""><br><br>
 								<h6>Our Advocacy Focus</h6>
-								<p>Culture and Arts, Economic Development, Education and Literacy, Environment, Good Governance, Health and Nutrition, Contemporary Issues.</p>
+								<p>Culture and Arts, Economic Development, Education and Literacy, Environment, Good
+									Governance,
+									Health and Nutrition, Contemporary Issues.</p>
 							</div>
 
 							<div class="col-4 sec-col">
 								<img src="/assets/images/placeholder.png" class="img-fluid" alt=""><br><br>
 								<h6>Our Mission</h6>
-								<p>NYLI is committed to equipping Negrosanon Youths through education, empowerment, and engagement–by providing them with safe spaces for action-driven dialogue, discussion, and self-discovery to become catalysts of change in their communities.</p>
+								<p>NYLI is committed to equipping Negrosanon Youths through education, empowerment, and
+									engagement–by providing them with safe spaces for action-driven dialogue,
+									discussion, and
+									self-discovery to become catalysts of change in their communities.</p>
 							</div>
-								
+
 							<div class="col-4 sec-col">
 								<img src="/assets/images/placeholder.png" class="img-fluid" alt=""><br><br>
 								<h6>Our Vision</h6>
-								<p>NYLI envisions empowered Negrosanon Youths who are committed towards a Sustainable Negros.</p>
+								<p>NYLI envisions empowered Negrosanon Youths who are committed towards a Sustainable
+									Negros.
+								</p>
 							</div>
 						</div>
 
@@ -111,13 +164,19 @@
 
 					<div class="section" id="section3">
 						<h1>Alumni</h1>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sit amet quam auctor, laoreet lacus a, malesuada massa. Nullam ultrices lectus quis neque eleifend, nec ullamcorper velit feugiat. Sed dignissim mauris id enim dictum, at malesuada elit aliquam. Nulla facilisi. Sed vulputate quam nec fringilla euismod.</p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sit amet quam auctor, laoreet
+							lacus
+							a, malesuada massa. Nullam ultrices lectus quis neque eleifend, nec ullamcorper velit
+							feugiat. Sed
+							dignissim mauris id enim dictum, at malesuada elit aliquam. Nulla facilisi. Sed vulputate
+							quam nec
+							fringilla euismod.</p>
 					</div>
 				</div>
-				
-			</div>
 
 			</div>
+
+		</div>
 		</div>
 	</section>
 
--- stories.php
+++ stories.php
@@ -1,142 +1,146 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-	<meta charset="UTF-8" />
-	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<title>NYLI</title>
-	<link rel="icon" type="image/x-icon" href="/assets/images/favicon.png" />
-	<link rel="stylesheet" href="assets/css/style.css" />
-	<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css" />
-	<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
-	<link href="assets/fontawesome/css/fontawesome.css" rel="stylesheet">
-	<link href="assets/fontawesome/css/brands.css" rel="stylesheet">
-	<link href="assets/fontawesome/css/solid.css" rel="stylesheet">
-</head>
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>NYLI</title>
+		<link rel="icon" type="image/x-icon" href="/assets/images/favicon.png" />
+		<link rel="stylesheet" href="assets/css/style.css" />
+		<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css" />
+		<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+		<link href="assets/fontawesome/css/fontawesome.css" rel="stylesheet">
+		<link href="assets/fontawesome/css/brands.css" rel="stylesheet">
+		<link href="assets/fontawesome/css/solid.css" rel="stylesheet">
+	</head>
 
-<body>
-	<?php require_once 'navbar.php'; ?>
+	<body>
+		<?php require_once 'navbar.php'; ?>
 
-	<section class="header">
-		<div class="d-flex justify-content-center p-5">
-			<div class="d-flex flex-column justify-content-center align-items-center py-4">
-				<div class="section-header-dark">
-					<h2>Stories</h2>
-					<hr>
-				</div>
-				<div class="p-2">
-				</div>
-			</div>
-		</div>
-	</section>
-
-	<section class="program-section">
-		<div class="container h-100 d-flex flex-column">
-			<div class="filter-content row mb-2">
-				<div class="col-6">
-					<select class="dropdown">
-						<option class="option-link" value="all">All</option>
-						<option class="option-link" value="category1">Category 1</option>
-						<option class="option-link" value="category2">Category 2</option>
-						<option class="option-link" value="category3">Category 3</option>
-					</select>
-				</div>
-				<div class="col-6">
-					<form class="search-form" action="#" style="float: right;">
-						<div class="search-field form-control">
-							<span class="fa fa-search search-icon"></span>
-							<input type="text" class="search form-control" placeholder="Search" name="search" autocomplete="off">
-						</div>
-						<button type="button" class="nyli-btn-primary-light">
-							<a href="#">Search</a>
-						</button>
-					</form>
-				</div>
-			</div>
-			<div class="publication-content">
-				<div class="row">
-					<div class="content-list col-4">
-						<img src="assets/images/3.png" class="img-fluid mx-auto d-block text-center">
-						<div class="header-title-main">
-							<h3>3 Negrosanon Young Leaders to represent Negros Occidental in a U.S. Fellowship</h3>
-							<p>January 24, 2023</p>
-						</div>
-						<button type="button" class="nyli-btn-primary-light">
-							<a href="story-article.html">Read more</a>
-						</button>
+		<section class="header">
+			<div class="d-flex justify-content-center p-5">
+				<div class="d-flex flex-column justify-content-center align-items-center py-4">
+					<div class="section-header-dark">
+						<h2>Stories</h2>
+						<hr>
 					</div>
-					<div class="content-list col-4">
-						<img src="assets/images/2.jpg" class="img-fluid mx-auto d-block text-center">
-						<div class="header-title-main">
-							<h3>“Relatively cheap” and “charging less for quality work” – How the Filipino online workers are being advertised by the Philippine business process outsourcing industry and online job sites</h3>
-							<p>January 24, 2023</p>
-						</div>
-						<button type="button" class="nyli-btn-primary-light">
-							<a href="story-article.html">Read more</a>
-						</button>
-					</div>
-					<div class="content-list col-4">
-						<img src="assets/images/beautiful.jpg" class="img-fluid mx-auto d-block text-center">
-						<div class="header-title-main">
-							<h3>NYLI, SK Federation Bacolod, capacitates the youth through Youth Employment Enrichment Workshop</h3>
-							<p>January 24, 2023</p>
-						</div>
-						<button type="button" class="nyli-btn-primary-light">
-							<a href="story-article.html">Read more</a>
-						</button>
-					</div>
-				</div>
-				<div class="row">
-					<div class="content-list col-4">
-						<img src="assets/images/default.png" class="img-fluid mx-auto d-block text-center">
-						<div class="header-title-main">
-							<h3>3 Negrosanon Young Leaders to represent Negros Occidental in a U.S. Fellowship</h3>
-							<p>January 24, 2023</p>
-						</div>
-						<button type="button" class="nyli-btn-primary-light">
-							<a href="story-article.html">Read more</a>
-						</button>
-					</div>
-					<div class="content-list col-4">
-						<img src="assets/images/default.png" class="img-fluid mx-auto d-block text-center">
-						<div class="header-title-main">
-							<h3>3 Negrosanon Young Leaders to represent Negros Occidental in a U.S. Fellowship</h3>
-							<p>January 24, 2023</p>
-						</div>
-						<button type="button" class="nyli-btn-primary-light">
-							<a href="story-article.html">Read more</a>
-						</button>
-					</div>
-					<div class="content-list col-4">
-						<img src="assets/images/default.png" class="img-fluid mx-auto d-block text-center">
-						<div class="header-title-main">
-							<h3>3 Negrosanon Young Leaders to represent Negros Occidental in a U.S. Fellowship</h3>
-							<p>January 24, 2023</p>
-						</div>
-						<button type="button" class="nyli-btn-primary-light">
-							<a href="story-article.html">Read more</a>
-						</button>
+					<div class="p-2">
 					</div>
 				</div>
 			</div>
-			<div id="pagination-container">
-				<ul id="pagination-list"></ul>
+		</section>
+
+		<section class="stories-section">
+			<div class="container d-flex flex-column">
+				<div class="filter-content row mb-2">
+					<div class="col-6">
+						<select class="dropdown">
+							<option class="option-link" value="all">All</option>
+							<option class="option-link" value="category1">Category 1</option>
+							<option class="option-link" value="category2">Category 2</option>
+							<option class="option-link" value="category3">Category 3</option>
+						</select>
+					</div>
+					<div class="col-6">
+						<form class="search-form" action="#" style="float: right;">
+							<div class="search-field form-control">
+								<span class="fa fa-search search-icon"></span>
+								<input type="text" class="search form-control" placeholder="Search" name="search"
+									autocomplete="off">
+							</div>
+							<button type="button" class="nyli-btn-primary-light">
+								<a href="#">Search</a>
+							</button>
+						</form>
+					</div>
+				</div>
+				<div class="stories-content">
+					<div class="row">
+						<div class="content-list col-4">
+							<img src="assets/images/3.png" class="img-fluid mx-auto d-block text-center">
+							<div class="header-title-main">
+								<h3>3 Negrosanon Young Leaders to represent Negros Occidental in a U.S. Fellowship</h3>
+								<p>January 24, 2023</p>
+							</div>
+							<button type="button" class="nyli-btn-primary-light">
+								<a href="story-article.php">Read more</a>
+							</button>
+						</div>
+						<div class="content-list col-4">
+							<img src="assets/images/2.jpg" class="img-fluid mx-auto d-block text-center">
+							<div class="header-title-main">
+								<h3>“Relatively cheap” and “charging less for quality work” – How the Filipino online
+									workers are being advertised by the Philippine business process outsourcing industry
+									and online job sites</h3>
+								<p>January 24, 2023</p>
+							</div>
+							<button type="button" class="nyli-btn-primary-light">
+								<a href="story-article.php">Read more</a>
+							</button>
+						</div>
+						<div class="content-list col-4">
+							<img src="assets/images/beautiful.jpg" class="img-fluid mx-auto d-block text-center">
+							<div class="header-title-main">
+								<h3>NYLI, SK Federation Bacolod, capacitates the youth through Youth Employment
+									Enrichment Workshop</h3>
+								<p>January 24, 2023</p>
+							</div>
+							<button type="button" class="nyli-btn-primary-light">
+								<a href="story-article.php">Read more</a>
+							</button>
+						</div>
+					</div>
+					<div class="row">
+						<div class="content-list col-4">
+							<img src="assets/images/default.png" class="img-fluid mx-auto d-block text-center">
+							<div class="header-title-main">
+								<h3>3 Negrosanon Young Leaders to represent Negros Occidental in a U.S. Fellowship</h3>
+								<p>January 24, 2023</p>
+							</div>
+							<button type="button" class="nyli-btn-primary-light">
+								<a href="story-article.php">Read more</a>
+							</button>
+						</div>
+						<div class="content-list col-4">
+							<img src="assets/images/default.png" class="img-fluid mx-auto d-block text-center">
+							<div class="header-title-main">
+								<h3>3 Negrosanon Young Leaders to represent Negros Occidental in a U.S. Fellowship</h3>
+								<p>January 24, 2023</p>
+							</div>
+							<button type="button" class="nyli-btn-primary-light">
+								<a href="story-article.php">Read more</a>
+							</button>
+						</div>
+						<div class="content-list col-4">
+							<img src="assets/images/default.png" class="img-fluid mx-auto d-block text-center">
+							<div class="header-title-main">
+								<h3>3 Negrosanon Young Leaders to represent Negros Occidental in a U.S. Fellowship</h3>
+								<p>January 24, 2023</p>
+							</div>
+							<button type="button" class="nyli-btn-primary-light">
+								<a href="story-article.php">Read more</a>
+							</button>
+						</div>
+					</div>
+				</div>
+				<div id="pagination-container">
+					<ul id="pagination-list"></ul>
+				</div>
 			</div>
-		</div>
-	</section>
+		</section>
 
-	<?php require_once 'footer.php'; ?>
+		<?php require_once 'footer.php'; ?>
 
-	<script src="/assets/bootstrap/js/bootstrap.bundle.js"></script>
-	<script src="/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
-	<script src="/assets/bootstrap/js/bootstrap.js"></script>
-	<script src="/assets/bootstrap/js/bootstrap.min.js"></script>
-	<script src="/assets/js/script.js"></script>
-	<script src="/assets/js/pagination.js"></script>
-</body>
+		<script src="/assets/bootstrap/js/bootstrap.bundle.js"></script>
+		<script src="/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+		<script src="/assets/bootstrap/js/bootstrap.js"></script>
+		<script src="/assets/bootstrap/js/bootstrap.min.js"></script>
+		<script src="/assets/js/script.js"></script>
+		<script src="/assets/js/pagination.js"></script>
+	</body>
 
-</html>
+</php>
